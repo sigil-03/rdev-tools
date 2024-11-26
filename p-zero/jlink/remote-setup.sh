2f472612259943088b4f6cf1b9acfa2d501b489c
sigil-03 <sigil@glyphs.tech>
2024-11-25 23:52:02 -0700

    add udev rules setup and re-organize file layout

--- p-zero/jlink/remote-setup.sh
+++ p-zero/jlink/remote-setup.sh
@@ -7,8 +7,8 @@ REMOTE_DEVICE=$1
 ssh ${REMOTE_USER}@$REMOTE_DEVICE mkdir -p ~/tmp
 
 # copy the device setup script to the temp directory
-rsync ./device-setup.sh ${REMOTE_USER}@${REMOTE_DEVICE}:~/tmp/
+rsync -r ./device ${REMOTE_USER}@${REMOTE_DEVICE}:~/tmp/
 
 # make setup script executable and execute
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} chmod +x ~/tmp/device-setup.sh 
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} . ~/tmp/device-setup.sh
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} chmod +x ~/tmp/device/device-setup.sh 
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} . ~/tmp/device/device-setup.sh
