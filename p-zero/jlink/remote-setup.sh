099db96e7f498a149c4ebc134c816e528748eca0
sigil-03 <sigil@glyphs.tech>
2024-11-27 11:19:11 -0700

    add tools directory creation

--- p-zero/jlink/remote-setup.sh
+++ p-zero/jlink/remote-setup.sh
@@ -4,7 +4,13 @@ REMOTE_USER=${USER}
 REMOTE_DEVICE=$1
 
 # make a remote temp directory
-ssh ${REMOTE_USER}@$REMOTE_DEVICE mkdir -p ~/tmp
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} mkdir -p ~/tmp
+
+# make a rdev user group
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} sudo groupadd fw-dev && sudo usermod -aG ${REMOTE_USER} rdev
+
+# make a remote tools directory
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} sudo mkdir /tools && sudo chgrp -R /tools && sudo chmod g+rwxs /tools
 
 # copy the device setup script to the temp directory
 rsync -r ./device ${REMOTE_USER}@${REMOTE_DEVICE}:~/tmp/
