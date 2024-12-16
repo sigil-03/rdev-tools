d264761432cdd0da77033cae1aa74b27feb465c9
sigil-03 <sigil@glyphs.tech>
2024-12-16 14:19:11 -0700

    fix remote setup script to use heredocs

--- p-zero/jlink/remote-setup.sh
+++ p-zero/jlink/remote-setup.sh
@@ -2,19 +2,35 @@
 
 REMOTE_USER=${USER}
 REMOTE_DEVICE=$1
-
+RDEV_GROUP="rdev"
 # make a remote temp directory
 ssh ${REMOTE_USER}@${REMOTE_DEVICE} mkdir -p ~/tmp
 
 # make a rdev user group
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} sudo groupadd fw-dev && sudo usermod -aG ${REMOTE_USER} rdev
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} << EOF 
+	sudo groupadd ${RDEV_GROUP} 
+	sudo usermod -aG ${RDEV_GROUP} ${REMOTE_USER}
+EOF
 
 # make a remote tools directory
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} sudo mkdir /tools && sudo chgrp -R /tools && sudo chmod g+rwxs /tools
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} << EOF
+	sudo mkdir /tools ;
+	sudo chgrp -R ${RDEV_GROUP} /tools ;
+	sudo chmod g+rwxs /tools ;
+EOF
+
+# make a remote env directory
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} << EOF
+	sudo mkdir /env ;
+	sudo chgrp -R ${RDEV_GROUP} /env ;
+	sudo chmod g+rwxs /env ;
+EOF
 
 # copy the device setup script to the temp directory
 rsync -r ./device ${REMOTE_USER}@${REMOTE_DEVICE}:~/tmp/
 
 # make setup script executable and execute
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} chmod +x ~/tmp/device/device-setup.sh 
-ssh ${REMOTE_USER}@${REMOTE_DEVICE} . ~/tmp/device/device-setup.sh
+ssh ${REMOTE_USER}@${REMOTE_DEVICE} << EOF
+	chmod +x ~/tmp/device/device-setup.sh ;
+	. ~/tmp/device/device-setup.sh ;
+EOF
